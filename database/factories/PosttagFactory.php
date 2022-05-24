ae5802d3d47e515f7facab9928edbcb4aa7245b1
anthony <diomede1991@libero.it>
2022-05-24 17:27:01 +0200

    copiato i database

---
+++ database/factories/PosttagFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\posttag;
+use Faker\Generator as Faker;
+
+$factory->define(posttag::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/migrations/2022_05_24_150645_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->timestamps();
+            $table->string('title',100);
+            $table->text('content');
+            $table->string('slug', 100)->unique();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
